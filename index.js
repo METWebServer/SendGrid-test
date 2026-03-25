1b5a0464244e08c8607e873d244acd0a50f2e6fb
METWebServer <45271742+METWebServer@users.noreply.github.com>
2026-03-25 16:37:28 -0500

    Update index.js
    Removed email from code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { createTransporter, sendMail } = require('./Email/setup');
     const transporter = createTransporter();
 
     const messageId = await sendMail(transporter, {
-      to: 'Luis.Granados@themetropolitanschool.com',
+      to: process.env.MAIL_TO,
       subject: 'Test email from Sendgrid/Paystub',
       text: 'Hello! Testing the new Sendgrid configuration.',
       html: `<p>Hello! This is an <b>HTML</b> email.</p>`,
