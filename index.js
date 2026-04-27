291cf63253ca2f1d042636890b184e9094d34c53
METWebServer <45271742+METWebServer@users.noreply.github.com>
2026-04-27 16:30:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { createTransporter, sendMail } = require('./Email/setup');
 
     const messageId = await sendMail(transporter, {
       to: process.env.MAIL_TO,
-      subject: 'Test email from Sendgrid/Paystub',
+      subject: 'Test email from Sendgrid',
       text: 'Hello! Testing the new Sendgrid configuration.',
       html: `<p>Hello! This is an <b>HTML</b> email.</p>`,
       // Optional extras:
